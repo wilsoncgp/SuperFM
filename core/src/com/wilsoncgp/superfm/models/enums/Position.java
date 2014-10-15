18608f7c78fdbb3788569c1463d97bb4253dacc1
liam.wilson <wilsoncgp@gmail.com>
2014-10-15 14:19:27 +0100

    Updated player to have Positions by creating an enum for Position.
    Player now has Array of Positions and a Main Position, which can be set from outside the player if the position is within the Array of Positions.

---
+++ core/src/com/wilsoncgp/superfm/models/enums/Position.java
@@ -0,0 +1,20 @@
+package com.wilsoncgp.superfm.models.enums;
+
+/**
+ * Created by liam.wilson on 15/10/2014.
+ */
+public enum Position {
+    GOALKEEPER,
+    RIGHTBACK,
+    CENTERBACK,
+    LEFTBACK,
+    DEFENSIVEMIDFIELDER,
+    CENTRALMIDFIELDER,
+    RIGHTMIDFIELDER,
+    LEFTMIDFIELDER,
+    ATTACKINGMIDFIELDER,
+    RIGHTWINGER,
+    LEFTWINGER,
+    CENTREFORWARD,
+    STRIKER
+}
--- core/src/com/wilsoncgp/superfm/models/people/Player.java
+++ core/src/com/wilsoncgp/superfm/models/people/Player.java
@@ -1,15 +1,45 @@
 package com.wilsoncgp.superfm.models.people;
 
+import com.badlogic.gdx.utils.Array;
 import com.wilsoncgp.superfm.models.base.Person;
+import com.wilsoncgp.superfm.models.enums.Position;
 
 import java.util.Date;
 
 /**
  * Created by Liam on 12/10/2014.
+ * Class representing a Player
  */
 public class Player extends Person {
 
-    public Player(String name, Date dateOfBirth) {
+    private Array<Position> positions;
+    private Position mainPosition;
+
+    /**
+     * Player constructor
+     * @param name Name of the player
+     * @param dateOfBirth Date of Birth of the player
+     * @param positions Array of Positions player can play in, first in array is considered main position
+     */
+    public Player(String name, Date dateOfBirth, Array<Position> positions) {
         super(name, dateOfBirth);
+        if(positions.size > 0) {
+            this.mainPosition = positions.first();
+            this.positions = positions;
+        }
+    }
+
+    /**
+     * Method to set the main position of the player. Position must be in Array of positions.
+     * @param position Position to set the Main Position to.
+     * @return Boolean value representing whether this method was successful or not. True if successful, false if not successful.
+     */
+    public boolean setMainPosition(Position position) {
+        if(this.positions.contains(position, true)) {
+            this.mainPosition = position;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
